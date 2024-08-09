c1d5910cb2250493132859d64a8d62871bac96b1
ldhlong <taja.long@yahoo.com>
2024-08-09 15:07:46 -0400

    1

--- db/migrate/20240607213430_add_user_id_to_users.rb
+++ db/migrate/20240607213430_add_user_id_to_users.rb
@@ -1,8 +1,7 @@
 # db/migrate/20240607213430_add_user_id_to_users.rb
 class AddUserIdToUsers < ActiveRecord::Migration[6.0]
   def change
-    change_table :users do |t|
-      t.primary_key :user_id
-    end
+    remove_column :users, :id, :primary_key
+    execute "ALTER TABLE users AND PRIMARY KEY (user_id);"
   end
 end
