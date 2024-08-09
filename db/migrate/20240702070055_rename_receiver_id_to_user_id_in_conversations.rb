5d056ab37a467bc7e47b9bb800635a8e8875550a
ldhlong <taja.long@yahoo.com>
2024-08-09 16:49:16 -0400

    1

--- db/migrate/20240702070055_rename_receiver_id_to_user_id_in_conversations.rb
+++ db/migrate/20240702070055_rename_receiver_id_to_user_id_in_conversations.rb
@@ -2,7 +2,6 @@ class RenameReceiverIdToUserIdInConversations < ActiveRecord::Migration[7.1]
   def change
     create_table :conversations do |t|
       add_column :conversations, :sender_id, :integer
-      add_column :conversations, :user_id, :integer
     end
   end
 end
