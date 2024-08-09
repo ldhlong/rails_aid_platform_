7d9973723f15bea39c27eaa23a7320f26a58acfa
ldhlong <taja.long@yahoo.com>
2024-08-09 02:36:34 -0400

    change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://backend-aid-b83f0dba9cf5.herokuapp.com/"
 
     resource "*",
       headers: :any,
