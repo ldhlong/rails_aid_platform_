bfa5f23865595818d25cbe1f38f31bd8efe4a247
ldhlong <taja.long@yahoo.com>
2024-08-09 15:44:04 -0400

    1

--- db/migrate/20240611033653_rename_id_column_to_user_id_in_help_requests.rb
+++
@@ -1,6 +0,0 @@
-class RenameIdColumnToUserIdInHelpRequests < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :requests, :user_id
-    rename_column :requests, :id, :user_id
-  end
-end
--- db/migrate/20240611034405_rename_id_column_to_user_id_in_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameIdColumnToUserIdInUsers < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :users, :id, :user_id
-  end
-end
