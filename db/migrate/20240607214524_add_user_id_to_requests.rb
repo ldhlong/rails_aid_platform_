a5fed31d17430939dc104767823b308d79bb992b
ldhlong <taja.long@yahoo.com>
2024-08-09 15:17:57 -0400

    1

--- db/migrate/20240607214524_add_user_id_to_requests.rb
+++ db/migrate/20240607214524_add_user_id_to_requests.rb
@@ -1,4 +1,4 @@
-class AddJtiToUsers < ActiveRecord::Migration[7.0]
+class AddUserIdToRequests < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :jti, :string, null: false
     add_index :users, :jti, unique: true
